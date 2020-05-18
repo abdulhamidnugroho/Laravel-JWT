e676a5fa469e85caa1f251a0b5b40136b2b5d834
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-05-18 10:31:49 +0700

    ++ ApiController Auth method

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use JWTAuth;
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterAuthRequest;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class ApiController extends Controller
+{
+    /**
+    * @var bool
+    */
+    public $loginAfterSignUp = true;
+
+    /**
+     * @param RegistrationAuthRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterAuthRequest $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        if ($this->loginAfterSignUp) {
+            return $this->login($request);
+        }
+
+        return response()->json([
+            'success'   =>  true,
+            'data'      =>  $user
+        ], 200);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $input = $request->only('email', 'password');
+        $token = null;
+
+        if (!$token = JWTAuth::attempt($input)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'token' => $token,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        try {
+            JWTAuth::invalidate($request->token);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (JWTException $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function getAuthUser(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        $user = JWTAuth::authenticate($request->token);
+
+        return response()->json(['user' => $user]);
+    }
+}
